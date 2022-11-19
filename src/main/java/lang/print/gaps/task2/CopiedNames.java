c1e6fd19dbb8f7efc9d33b3f13646ef59ff63bca
myshukurov2002 <107299390+myshukurov2002@users.noreply.github.com>
2022-11-20 00:25:07 +0500

    Update CopiedNames.java

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,17 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-       int morrow2 = 0;
-        int $money = 10;
-        int hundred = 100;
-        int back = 1000;
-        int _underscore = 10000;
-        int sharp = 100000;
-        int star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+       int variable = 0;
+        int _variable = 2000;
+        int value = 1000;
+        int $variable = 4000;
+        int value2 = 3000;
+        int variables = 5000;
+        variables = 7000;
+        int value1 = 90000;
+        int _int = 100000;
+        int myinteger = 1000000;
+        int myvar = 1234567;
+        int my____integer = 1234567;
     }
 }
